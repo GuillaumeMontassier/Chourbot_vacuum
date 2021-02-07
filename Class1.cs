29d44fe2336fc5bb38555c2669652371b47e0340
RiwanLegal <rlegbzh@gmail.com>
2021-02-07 14:22:24 +0100

    Spawn dust Jewelry random

---
+++ Class1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Chourbot_vacuum
+{
+    public class Case
+    {
+        private bool jewelry = false;
+        private bool dust = false;
+        private String case_name = "";
+        public Button button = new Button();
+
+        public bool get_jewelry_status()
+        {
+            return jewelry;
+        }
+        public void spawn_jewelry()
+        {
+            jewelry = true;
+            case_text();
+        }
+        public void clean_jewelry()
+        {
+            jewelry = false;
+            case_text();
+        }
+
+        public bool get_dust_status()
+        {
+            return dust;
+        }
+        public void spawn_dust()
+        {
+            dust = true;
+            case_text();
+        }
+        public void clean_dust()
+        {
+            dust = false;
+            case_text();
+        }
+
+        public void case_text()
+        {
+            if ((jewelry == true) && (dust== true))
+            {
+                button.Text = "jewelry dust";
+            }
+            else if (jewelry && !dust)
+            {
+                button.Text = "jewelry";
+            }
+            else if (!jewelry && dust)
+            {
+                button.Text = "dust";
+            }
+            else
+            {
+                button.Text = "";
+            }
+        }
+    }
+}
--- Form1.cs
+++ Form1.cs
@@ -12,9 +12,75 @@ namespace Chourbot_vacuum
 {
     public partial class Form1 : Form
     {
+        Timer t = new Timer
+        {
+            Interval = 2000
+        };
+
+        List<Case> cases = new List<Case>();
+
+        List<String> type_object = new List<String> { "Poussière", "bijou" };
+
         public Form1()
         {
             InitializeComponent();
+            foreach (var button in this.Controls.OfType<Button>())
+            {
+                button.Enabled = false;
+                Case new_case = new Case();
+                new_case.button = button;
+                cases.Add(new_case);
+            }
+            t.Tick += new EventHandler(timer_tick);
+            t.Start();
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // Génération aléatoire d'objet
+            generate_Object();
+        }
+
+        private void timer_tick(object sender, EventArgs e)
+        {
+            generate_Object();
+            var random = new Random();
+            t.Interval = random.Next(5000, 10000);
+        }
+
+        private void generate_Object()
+        {
+            var random = new Random();
+            // Random Case Selection
+            int index_random_case = random.Next(cases.Count);
+
+            // Random Object Selection
+            int index_random_object_type = random.Next(type_object.Count);
+
+            if (index_random_object_type == 0)
+            {
+                cases[index_random_case].spawn_dust();
+            }
+            else
+            {
+                cases[index_random_case].spawn_jewelry();
+            }
+        }
+
+        private void restartToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            foreach (var a_case in cases)
+            {
+                {
+                    a_case.clean_dust();
+                    a_case.clean_jewelry();
+                }
+            }
         }
     }
 }
